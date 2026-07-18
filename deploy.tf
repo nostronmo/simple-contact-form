6a22b016e74389aab47662769ba7953ed563f41c
pogeku <pogeku@lapiz>
2026-07-18 17:34:34 -0300

    Weh weh

--- deploy.tf
+++ deploy.tf
@@ -27,10 +27,10 @@ resource "aws_s3_bucket" "terraform_state" {
   }
 }
 
-# tfsec:ignore:aws-s3-encryption-customer-key
 resource "aws_s3_bucket_server_side_encryption_configuration" "state_encryption" {
   bucket = aws_s3_bucket.terraform_state.id
   rule {
+    # tfsec:ignore:aws-s3-encryption-customer-key
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
     }
