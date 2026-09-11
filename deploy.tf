5898dda8467f2ecfa73d47c7e8bcd3eb4745ddc5
pogeku <samuelsantana778@gmail.com>
2026-09-11 13:53:57 -0300

    Small changes

--- deploy.tf
+++ deploy.tf
@@ -37,20 +37,20 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "state_encryption"
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "state_public_block" {
-  bucket                  = aws_s3_bucket.terraform_state.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
+#resource "aws_s3_bucket_public_access_block" "state_public_block" {
+#  bucket                  = aws_s3_bucket.terraform_state.id
+#  block_public_acls       = true
+#  block_public_policy     = true
+#  ignore_public_acls      = true
+#  restrict_public_buckets = true
+#}
 
-resource "aws_s3_bucket_versioning" "state_versioning" {
-  bucket = aws_s3_bucket.terraform_state.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
+#resource "aws_s3_bucket_versioning" "state_versioning" {
+#  bucket = aws_s3_bucket.terraform_state.id
+#  versioning_configuration {
+#    status = "Enabled"
+#  }
+#}
 
 resource "tls_private_key" "ssh_key" {
   algorithm = "RSA"
